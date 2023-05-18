84aa42042f1e7fb0806b31b93121cf365929bbbd
Koome <judekoome360@gmail.com>
2023-05-18 09:50:36 +0300

    complete with commits

---
+++ array.rb
@@ -0,0 +1,24 @@
+numbers = [5, 12, 8, 3, 15, 10]
+
+#Array Methods
+
+# 1. The sum of all numbers
+sum = numbers.sum
+puts sum
+
+# 2. The maximum number
+max = numbers.max
+puts max
+
+# 3. The minimum number
+minimum = numbers.min
+puts = minimum
+
+# 4. The average of all numbers
+average = sum / numbers.length.to_f
+puts average
+
+# 5. Whether any number is greater than 10
+result = numbers.any? { |number| number > 10 }
+puts result
+
---
+++ hash.rb
@@ -0,0 +1,21 @@
+scores = {
+    "Alice" => 85,
+    "Bob" => 92,
+    "Charlie" => 78,
+    "David" => 90,
+    "Eve" => 88
+  }
+
+  # 1. The student with the highest score
+  
+  top_student = scores.key(scores.values.max)
+  puts "The student with the highest score is #{top_student} with a score of #{scores[top_student]}."
+
+  # 2. The student with the lowest score
+  
+  low_student = scores.key(scores.values.min)
+  puts "The lowest student is #{low_student}with a score of #{scores[low_student]}"
+
+  # 3. The average score of all students
+  avg_score = scores.values.sum / scores.values.length
+  puts "The average score of all students is #{avg_score}"
