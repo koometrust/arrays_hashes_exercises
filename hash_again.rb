a82b78ba2cc3af4e0ac58f90de2b50302406fdeb
Koome <judekoome360@gmail.com>
2023-05-19 11:36:54 +0300

    solution for hash that is simpler

---
+++ hash_again.rb
@@ -0,0 +1,54 @@
+
+scores = {
+    "Alice" => 85,
+    "Bob" => 92,
+    "Charlie" => 78,
+    "David" => 90,
+    "Eve" => 88
+  }
+  
+  # 1. The student with the highest score
+  highest_scorer = scores.max_by { |student,score| score}
+  puts "The highest scoring student is #{highest_scorer[0]}"
+  
+  # 2. The student with the lowest score
+  lowest_scorer = scores.min_by {|student, score| score}
+  puts "The lowest scoring student is #{lowest_scorer[0]}"
+
+  # 3. The average score of all students
+  sum_of_scores = scores.values.sum
+  average = sum_of_scores.to_f / scores.length
+  puts "The average score of the students is #{average}"
+
+
+
+
+
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
